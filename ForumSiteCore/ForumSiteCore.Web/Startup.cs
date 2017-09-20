e1ca2ac254455e44f1163dd906dba1f43ab0820c
Kristopher Glassmire <kglassmire@gmail.com>
2017-09-20 19:22:27 -0400

    Some view fixes and options in startup scaffolded but not really used.

--- ForumSiteCore/ForumSiteCore.Web/Startup.cs
+++ ForumSiteCore/ForumSiteCore.Web/Startup.cs
@@ -35,10 +35,34 @@ namespace ForumSiteCore.Web
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            ConfigureIdentity(services);
+            ConfigureCookieSettings(services);
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
+        }
+
+        private void ConfigureCookieSettings(IServiceCollection services)
+        {
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                //options.Cookie.Expiration = TimeSpan.FromDays(150);
+                //options.LoginPath = "/Account/Login"; // If the LoginPath is not set here, ASP.NET Core will default to /Account/Login
+                //options.LogoutPath = "/Account/Logout"; // If the LogoutPath is not set here, ASP.NET Core will default to /Account/Logout
+                //options.AccessDeniedPath = "/Account/AccessDenied"; // If the AccessDeniedPath is not set here, ASP.NET Core will default to /Account/AccessDenied
+                //options.SlidingExpiration = true;
+            });
+        }
+
+        private void ConfigureIdentity(IServiceCollection services)
+        {
+            services.Configure<IdentityOptions>(options =>
+            {
+                // configure stuff here
+                
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
