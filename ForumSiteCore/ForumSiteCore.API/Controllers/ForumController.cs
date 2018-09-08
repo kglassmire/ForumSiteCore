d7e9225d36b58765d6797be934fd15a597130d48
Kristopher Glassmire <kglassmire@gmail.com>
2018-09-08 16:51:05 -0400

    Exception handler now camelCasing exception json (will help me when developing this)

--- ForumSiteCore/ForumSiteCore.API/Controllers/ForumController.cs
+++ ForumSiteCore/ForumSiteCore.API/Controllers/ForumController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ForumSiteCore.Business.Models;
 using ForumSiteCore.Business.ViewModels;
 using ForumSiteCore.Business.Services;
 using Microsoft.AspNetCore.Http;
--- ForumSiteCore/ForumSiteCore.API/Middleware/ExceptionHandler.cs
+++ ForumSiteCore/ForumSiteCore.API/Middleware/ExceptionHandler.cs
@@ -7,17 +7,23 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using ForumSiteCore.Business.Responses;
+using Newtonsoft.Json.Serialization;
+using System.Dynamic;
 
 namespace ForumSiteCore.API.Middleware
 {
     // most of the credit for this goes to this: https://stackoverflow.com/a/48625298
     public class ExceptionHandler
     {
+    
         private readonly RequestDelegate _next;
+        private static JsonSerializerSettings _jsonSettings;
 
         public ExceptionHandler(RequestDelegate next)
         {
             _next = next;
+            ConfigureJsonSerializerSettings();
+
         }
 
         public async Task Invoke(HttpContext context)
@@ -32,13 +38,41 @@ namespace ForumSiteCore.API.Middleware
             }
         }
 
+        private void ConfigureJsonSerializerSettings()
+        {
+            _jsonSettings = new JsonSerializerSettings();
+            var contractResolver = new CamelCasePropertyNamesContractResolver();
+            // turns out there are self-reference loops on these objects frequently. Looks like I am going to have to deal with a lack of camel casing.
+            //contractResolver.IgnoreSerializableInterface = false;
+            //contractResolver.IgnoreSerializableAttribute = false;
+            _jsonSettings.ContractResolver = contractResolver;
+        }
+
         private async Task HandleExceptionAsync(HttpContext context, Exception exception)
         {
             var response = context.Response;
             response.ContentType = "application/json";
             response.StatusCode = (int)HttpStatusCode.InternalServerError;
-            Log.Error(exception, "An error occurred");
-            await response.WriteAsync(JsonConvert.SerializeObject(new FiveHundredErrorResponse() { Data = exception, Message = "An error occurred", Status = "error" }));
+            
+            Log.Error(exception, "An unexpected server error occurred");
+            String json = String.Empty;
+            try
+            {
+                json = JsonConvert.SerializeObject(new FiveHundredErrorResponse()
+                {
+                    Data = exception,
+                    Message = "An unexpected server error occurred",
+                    Status = "error"
+                }, _jsonSettings);
+            }
+            catch(Exception e)
+            {
+                Log.Error(e, "Error while attempting to serialize 500 exception for response. Returning blank instead.");
+            }
+            finally
+            {
+                await response.WriteAsync(json);
+            }            
         }
     }
 }
